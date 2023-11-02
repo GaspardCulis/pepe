8956e8eca9c12dffbc404b2ad67aa3c9dcced20d
GaspardCulis <gasdev.lp@gmail.com>
2023-11-02 16:48:34 +0100

    Added EmailJS integration to TestimonyForm

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+	readonly PUBLIC_EMAILJS_PUBLIC_KEY: string;
+	readonly PUBLIC_EMAILJS_SERVICE_ID: string;
+	readonly PUBLIC_EMAILJS_TEMPLATE_ID: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
