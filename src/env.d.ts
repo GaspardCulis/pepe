76697fff3a0e49a9c07dbf0d2cd503e2c5ba018e
GaspardCulis <gasdev.lp@gmail.com>
2023-11-03 11:16:19 +0100

    Added mailto link In case of MailJS failure

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,8 @@ interface ImportMetaEnv {
 	readonly PUBLIC_EMAILJS_PUBLIC_KEY: string;
 	readonly PUBLIC_EMAILJS_SERVICE_ID: string;
 	readonly PUBLIC_EMAILJS_TEMPLATE_ID: string;
+
+	readonly PUBLIC_CONTACT_MAIL: string;
 }
 
 interface ImportMeta {
