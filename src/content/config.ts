7d730a421f75a847f6dfd9797fc384ffa462890a
GaspardCulis <gasdev.lp@gmail.com>
2023-11-25 16:50:58 +0100

    Added galerie contentCollection definition

---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+import { defineCollection, z } from "astro:content";
+
+const galeryCollection = defineCollection({
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			image: image().refine((img) => img.width >= 1080, {
+				message: "Cover image must be at least 1080 pixels wide!",
+			}),
+			imageAlt: z.string(),
+			category: z.enum(["jarre", "modelage", "cristallisation"]),
+		}),
+});
+
+export const collections = {
+	galerie: galeryCollection,
+};
