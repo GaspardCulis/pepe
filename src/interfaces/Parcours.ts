1d19b5e9ec4b399770f7c4664e82b1121b99879c
GaspardCulis <gasdev.lp@gmail.com>
2023-11-29 22:16:12 +0100

    Removed old unused interfaces

--- src/interfaces/Parcours.ts
+++
@@ -1,6 +0,0 @@
-export default interface Parcours {
-	id: number;
-	attributes: {
-		content: string;
-	};
-}
--- src/interfaces/Testimony.ts
+++
@@ -1,8 +0,0 @@
-export default interface Testimony {
-	id: number;
-	attributes: {
-		author: string;
-		author_role: string;
-		content: string;
-	};
-}
