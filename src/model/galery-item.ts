c13c1cee7813e4112ac996bdeeba46daaef00cdd
GaspardCulis <gasdev.lp@gmail.com>
2023-12-01 14:08:17 +0100

    Added id property and get(id) to GaleryItem model

--- src/model/galery-item.ts
+++ src/model/galery-item.ts
@@ -6,30 +6,30 @@ import Image from "./image";
 
 export default class GaleryItem {
 	private constructor(
+		readonly id: number,
 		readonly title: string,
 		readonly description: string,
 		readonly image: Image,
 	) {}
 
-	public static async getMatchingCategory(
-		category: string, // No strong typing :(
-		options?: {
-			size?: StrapiImageSize;
-		},
-	): Promise<GaleryItem[]> {
+	public static async getAll(options: {
+		query?: Record<string, string>;
+		size?: StrapiImageSize;
+	}): Promise<GaleryItem[]> {
 		let out: GaleryItem[] = [];
 
 		const results = await fetchStrapiApi({
 			endpoint: "galery-items",
 			query: {
 				populate: "image",
-				"filters[category][slug][$eq]": category,
+				...options.query,
 			},
 		});
 
 		for (let result of results.data) {
 			out.push(
 				new GaleryItem(
+					result.id,
 					result.attributes.title,
 					result.attributes.description,
 					toImage({
@@ -42,5 +42,35 @@ export default class GaleryItem {
 		}
 
 		return out;
+	}
+
+	public static async get(
+		id: number,
+		options: {
+			size?: StrapiImageSize;
+		},
+	): Promise<GaleryItem> {
+		const result = await this.getAll({
+			...options,
+			query: {
+				"filters[id][$eq]": `${id}`,
+			},
+		});
+
+		return result[0];
+	}
+
+	public static async getMatchingCategory(
+		category: string, // No strong typing :(
+		options?: {
+			size?: StrapiImageSize;
+		},
+	): Promise<GaleryItem[]> {
+		return await this.getAll({
+			...options,
+			query: {
+				"filters[category][slug][$eq]": category,
+			},
+		});
 	}
 }
