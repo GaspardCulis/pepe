38f46d537b312a1ca076e6f40ac77809e30ad709
GaspardCulis <gasdev.lp@gmail.com>
2024-02-18 16:04:39 +0100

    Hero + HomeQuote: Now uses client:tina directive

--- src/components/cms/HomeQuote.tsx
+++ src/components/cms/HomeQuote.tsx
@@ -1,16 +1,12 @@
 import { useTina, tinaField } from "tinacms/dist/react";
 import { TinaMarkdown } from "tinacms/dist/rich-text";
 
-export const HomeQuote = (props: { query: string, variables:object, data: any }) => {
+export const HomeQuote = (props: { query: string, variables: object, data: any }) => {
   const { data } = useTina(props);
 
   return (
-      <div
-  				data-tina-field={tinaField(data, "home")}
-        >
-  			<TinaMarkdown
-  				content={data.home.quote}
-  			/>
-  		</div>
+    <TinaMarkdown
+      content={data.home.quote}
+    />
   );
 };
