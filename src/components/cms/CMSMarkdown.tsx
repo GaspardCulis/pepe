d6421a5a5413b36d1125bbdba0ef6240b70070ef
GaspardCulis <gasdev.lp@gmail.com>
2024-02-18 16:16:13 +0100

    Fixed CMSMarkdown imports not being updated after previous commit

--- src/components/cms/CMSMarkdown.tsx
+++ src/components/cms/CMSMarkdown.tsx
@@ -1,12 +1,14 @@
 import { useTina, tinaField } from "tinacms/dist/react";
 import { TinaMarkdown } from "tinacms/dist/rich-text";
 
-export const HomeQuote = (props: { query: string, variables: object, data: any }) => {
+export const CMSMarkdown = (props: { query: string, variables: object, data: any }) => {
   const { data } = useTina(props);
 
   return (
-    <TinaMarkdown
-      content={data.home.quote}
-    />
+    <div data-tina-field={tinaField(data)}>
+      <TinaMarkdown
+        content={data.home.quote}
+      />
+    </div>
   );
 };
