97477342a536f242e72e532a62ed66d1be9d9698
GaspardCulis <gasdev.lp@gmail.com>
2024-02-25 22:13:24 +0100

    Create AuthProvider.ts, custom TinaCMS auth provider

---
+++ lib/AuthProvider.ts
@@ -0,0 +1,39 @@
+import { AbstractAuthProvider } from "tinacms";
+import LogtoClient from "@logto/browser";
+import { createLogToClient } from "./utils";
+
+export default class AuthProvider extends AbstractAuthProvider {
+	private _logtoClient: LogtoClient | undefined;
+
+	constructor() {
+		super();
+	}
+
+	private getLogToClient(): LogtoClient {
+		if (!this._logtoClient) {
+			this._logtoClient = createLogToClient();
+		}
+		return this._logtoClient;
+	}
+
+	async getToken(): Promise<any> {
+		const accessToken = await this.getLogToClient().getAccessToken(
+			"https://danielculis.fr/api/tina/gql",
+		);
+		return { id_token: accessToken };
+	}
+
+	async getUser(): Promise<boolean> {
+		return this.getLogToClient().isAuthenticated();
+	}
+
+	async logout(): Promise<any> {
+		return this.getLogToClient().signOut();
+	}
+
+	async authenticate(
+		props?: Record<string, string> | undefined,
+	): Promise<any> {
+		this.getLogToClient().signIn("http://localhost:4321/api/tina/auth");
+	}
+}
