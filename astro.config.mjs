84a03085f4532e3db9ca0cbfea5eaedff7fed59a
GaspardCulis <gasdev.lp@gmail.com>
2024-02-25 22:43:37 +0100

    astro.config: Now using vercel SSR adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 const tina = ({ directiveName = "tina" } = {}) => ({
 	name: "tina-cms",
@@ -22,7 +22,5 @@ export default defineConfig({
 		"/admin": "/admin/index.html",
 	},
 	integrations: [tailwind(), react(), tina()],
-	adapter: node({
-		mode: "standalone",
-	}),
+	adapter: vercel(),
 });
