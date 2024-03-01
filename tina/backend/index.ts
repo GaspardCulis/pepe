4ee3562d7f206cb37cdc6f81ab746c3fa3b4b2d1
GaspardCulis <gasdev.lp@gmail.com>
2024-03-01 11:42:05 +0100

    backand -> index: Now using auth plugin

--- tina/backend/index.ts
+++ tina/backend/index.ts
@@ -1,11 +1,19 @@
 import { Elysia } from "elysia";
 import { cors } from "@elysiajs/cors";
 
+import { auth } from "./plugins/auth";
+
 import * as gql from "./routes/gql";
 import * as media from "./routes/media";
 
 new Elysia()
 	.use(cors({ methods: "*" }))
+	.use(
+		auth({
+			remote_url: "https://auth.gasdev.fr/oidc/jwks",
+			issuer_url: "https://auth.gasdev.fr/oidc",
+		}),
+	)
 	.group("/api", (app) =>
 		app
 			.post("/gql", gql.POST)
