dc5eb48caf4302e4a12130eeba573a23d45444f2
GaspardCulis <gaspard@gasdev.fr>
2025-01-04 13:48:32 +0100

    chore(astro): Clean up config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-
-const tina = ({ directiveName = "tina" } = {}) => ({
-	name: "tina-cms",
-	hooks: {
-		"astro:config:setup": ({ addClientDirective }) => {
-			addClientDirective({
-				name: directiveName,
-				entrypoint: "./client-directives/tina.mjs",
-			});
-		},
-	},
-});
 
 // https://astro.build/config
 export default defineConfig({
 	output: "static",
-	integrations: [tailwind(), react(), tina()],
+	integrations: [tailwind()],
 });
