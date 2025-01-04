add9ae66290c4ddcd3d555845cd46d155364e0be
GaspardCulis <gaspard@gasdev.fr>
2025-01-04 15:01:34 +0100

    chore(astro): Added MDX integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-	output: "static",
-	integrations: [tailwind()],
-	image: {
-		domains: ["pepe-cms.s3web.gasdev.fr"]
-	}
+    output: "static",
+    integrations: [tailwind(), mdx()],
+    image: {
+        domains: ["pepe-cms.s3web.gasdev.fr"]
+    }
 });
