9b92556d17ccce0d96dadf38f44f61ea435708c6
GaspardCulis <gaspard@gasdev.fr>
2026-06-04 17:02:48 +0200

    fix(content.config.ts): Update astro content API usage to v6

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,33 +1,34 @@
+import { defineCollection } from "astro:content"
 import { glob } from 'astro/loaders';
-import { defineCollection, z } from 'astro:content';
+import { z } from 'astro/zod';
 
 const categories = defineCollection({
-    loader: glob({ pattern: "**/*.md", base: "./src/content/categories" }),
-    schema: z.object({
-        name: z.string(),
-        vignette: z.string(), // URL to CMS
-        items: z.array(z.object({
-            name: z.string(),
-            image: z.string(), // URL to CMS
-            description: z.string()
-        }))
-    })
+  loader: glob({ pattern: "**/*.md", base: "./src/content/categories" }),
+  schema: z.object({
+    name: z.string(),
+    vignette: z.string(), // URL to CMS
+    items: z.array(z.object({
+      name: z.string(),
+      image: z.string(), // URL to CMS
+      description: z.string()
+    }))
+  })
 });
 
 const story = defineCollection({
-    loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/story" }),
-    schema: z.object({
-        slug: z.string(),
-        title: z.string()
-    })
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/story" }),
+  schema: z.object({
+    slug: z.string(),
+    title: z.string()
+  })
 });
 
 const testimonies = defineCollection({
-    loader: glob({ pattern: "**/*.md", base: "./src/content/testimonies" }),
-    schema: z.object({
-        author: z.string(),
-        date: z.coerce.date().optional(),
-    })
+  loader: glob({ pattern: "**/*.md", base: "./src/content/testimonies" }),
+  schema: z.object({
+    author: z.string(),
+    date: z.coerce.date().optional(),
+  })
 });
 
 export const collections = { categories, story, testimonies };
