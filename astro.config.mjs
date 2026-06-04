b90b2f56f956f2ecac68382a497bb7521eb5d571
GaspardCulis <gaspard@gasdev.fr>
2026-06-04 23:29:11 +0200

    fix: Fix deps conflicts and migrate to tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 
 import mdx from "@astrojs/mdx";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-    output: "static",
-    integrations: [tailwind(), mdx()],
-    image: {
-        domains: ["pepe-cms.s3web.gasdev.fr"]
-    }
+  output: "static",
+  integrations: [mdx()],
+
+  image: {
+    domains: ["pepe-cms.s3web.gasdev.fr"]
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
